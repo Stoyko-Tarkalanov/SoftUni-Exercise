f0ffdca6d14161b22b9448e552e1a24c0565e983
Stoiko Tarkalanov <76123758+StoikoTarkalanov@users.noreply.github.com>
2021-02-26 13:51:11 +0200

    ASYNCHRONOUS PROGRAMMING

---
+++ ASYNCHRONOUS PROGRAMMING/04/app.js
@@ -0,0 +1,69 @@
+// function solve() {
+//     const url = `http://localhost:3030/jsonstore/cookbook/recipes`;
+
+//     const mainElement = document.querySelector('main');
+
+//     fetch(url)
+//         .then(response => response.json())
+//         .then(recipes => {
+//             mainElement.innerHTML = '';
+//             Object.values(recipes).forEach(r => {
+//                 const result = e('article', { className: 'preview' },
+//                     e('div', { className: 'title' }, e('h2', {}, r.name)),
+//                     e('div', { className: 'small' }, e('img', { src: r.img }))
+//                 );
+//                 mainElement.appendChild(result);
+//             });
+//         })
+//         .catch(error => alert(error.message));
+// }
+
+async function solve() {
+    const url = `http://localhost:3030/jsonstore/cookbook/recipes`;
+
+    const mainElement = document.querySelector('main');
+
+    try {
+
+        const response = await fetch(url);
+        const recipes = await response.json();
+        mainElement.innerHTML = '';
+        Object.values(recipes).forEach(r => {
+            const result = e('article', { className: 'preview' },
+                e('div', { className: 'title' }, e('h2', {}, r.name)),
+                e('div', { className: 'small' }, e('img', { src: r.img }))
+            );
+            mainElement.appendChild(result);
+        });
+    } catch (error) {
+        alert(error.message);
+    }
+
+}
+
+window.addEventListener('load', () => solve());
+
+function e(type, attributes, ...content) {
+    const result = document.createElement(type);
+
+    for (let [attr, value] of Object.entries(attributes || {})) {
+        if (attr.substring(0, 2) == 'on') {
+            result.addEventListener(attr.substring(2).toLocaleLowerCase(), value);
+        } else {
+            result[attr] = value;
+        }
+    }
+
+    content = content.reduce((a, c) => a.concat(Array.isArray(c) ? c : [c]), []);
+
+    content.forEach(e => {
+        if (typeof e == 'string' || typeof e == 'number') {
+            const node = document.createTextNode(e);
+            result.appendChild(node);
+        } else {
+            result.appendChild(e);
+        }
+    });
+
+    return result;
+}
